6f37e9d63798d4617ef9dc22724dc2b61efdb667
rafaelledo <rafaeloliveiraledo@gmail.com>
2022-10-03 19:38:06 -0300

    2nd commit
    first exercise

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,19 +1,18 @@
 import './App.css'
 import React from 'react'
-import { BrowserRouter as Router } from  'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
 
 const App = props => {
-
     return (
-                <div className="App">
-                    <Router>
-                        <Menu />
-                        <Content />
-                    </Router>
-                </div>
+        <div className="App">
+            <Router>
+                <Menu />
+                <Content />
+            </Router>
+        </div>
     )
 }
 
