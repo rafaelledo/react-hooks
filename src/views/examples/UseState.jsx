acaf05c837f5517d5ddd2b4a128cc0e3239e2a8f
rafaelledo <rafaeloliveiraledo@gmail.com>
2022-10-03 19:52:31 -0300

    3rd commit
    second exercise

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -3,27 +3,30 @@ import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState("Inicial...")
 
-    return (
-        <div className="UseState">
-            <PageTitle
-                title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
+  return (
+    <div className="UseState">
+      <PageTitle
+        title="Hook UseState"
+        subtitle="Estado em componentes funcionais!"
+      />
 
-            <SectionTitle title="Execício #01" />
-            <div className="center">
-                <span className="text">{count}</span>
-                <div>
-                    <button className="btn" onClick={ () => setCount(count - 1) } >-1</button>
-                    <button className="btn" onClick={ () => setCount(count + 1) } >+1</button>
-                </div>
-            </div>
-
-            <SectionTitle title="Execício #02" />
+      <SectionTitle title="Execício #01" />
+      <div className="center">
+        <span className="text">{count}</span>
+        <div>
+          <button className="btn" onClick={() => setCount(count - 1)} >-1</button>
+          <button className="btn" onClick={() => setCount(count + 1)} >+1</button>
         </div>
-    )
+      </div>
+
+      <SectionTitle title="Execício #02" />
+      <input type="text" className="input"
+        value={name} onChange={e => setName(e.target.value)} />
+    </div>
+  )
 }
 
 export default UseState
